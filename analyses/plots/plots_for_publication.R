5eee99fe29e02773d90ef7aa99f8381d2f8e0f7b
ianhussey <ian.hussey@ugent.be>
2020-06-15 17:46:12 +0200

    added exclusions for participants who recieved faking instructions

--- analyses/plots/plots_for_publication.R
+++ analyses/plots/plots_for_publication.R
@@ -17,7 +17,7 @@ pdf(NULL)
 dev.control(displaylist = "enable")
 par(mfrow=c(2,1))
 layout(matrix(c(1,2), 2, 1, byrow = TRUE),
-       widths=c(1), heights=c(2.65,1))
+       widths=c(1), heights = c(1.81,1))
 
 metafor::forest(fit_internal_consistency_permuted_estimates,
                 transf = transf.iabt,
@@ -32,20 +32,20 @@ text(1.6, 27, bquote(paste(alpha, " [95% CI]")), pos = 2)
 
 metafor::forest(fit_test_retest_icc,
                 transf = transf.ztor,
-                xlab = "Interclass Correlation Coefficient",
+                xlab = "Absolute Agreement Interclass Correlation",
                 addcred = TRUE,
                 refline = FALSE,
-                xlim = c(-1, 1.6),
-                at = c(0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
+                xlim = c(-1.78, 1.82),
+                at = c(-0.4, -0.2, 0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
                 mlab = add_heterogeneity_metrics_to_forest(fit_test_retest_icc))
-text(-1, 4, "Test-Retest Reliability", pos = 4)
-text(1.6, 4, "ICC [95% CI]", pos = 2)
+text(-1.78, 10, "Test-Retest Reliability", pos = 4)
+text(1.82, 10, "ICC [95% CI]", pos = 2)
 
 p1 <- recordPlot()
 invisible(dev.off())
 
 pdf("forest_plots.pdf",
-    width = 8, 
-    height = 12)
+    width = 7, 
+    height = 10)
 p1
 dev.off()
