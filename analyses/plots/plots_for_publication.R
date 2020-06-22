93da2c8b895e753874d0640c3d9903792b273460
ianhussey <ian.hussey@ugent.be>
2020-06-23 01:13:20 +0200

    added Chad's wave 2 data, updated processing, analyses

--- analyses/plots/plots_for_publication.R
+++ analyses/plots/plots_for_publication.R
@@ -2,6 +2,8 @@
 library(tidyverse)
 library(metafor)
 library(patchwork)
+library(ggExtra)
+library(scales) 
 
 add_heterogeneity_metrics_to_forest <- function(fit) {
   bquote(paste("RE Model (", tau^2, " = ", .(formatC(round(fit$tau2, 1))), 
@@ -40,9 +42,9 @@ metafor::forest(fit_internal_consistency_permuted_estimates,
                 #ilab = data_internal_consistency_permuted_estimates$n,
                 #ilab.xpos = 1.1,
                 #ilab.pos = 2)
-text(-1, 28, "Internal consistency", pos = 4)
-text(1.6, 28, bquote(paste(alpha, " [95% CI]")), pos = 2)
-#text(1.1, 28, expression(italic("N")), pos = 2)
+text(-1, 39, "Internal consistency", pos = 4)
+text(1.6, 39, bquote(paste(alpha, " [95% CI]")), pos = 2)
+#text(1.1, 39, expression(italic("N")), pos = 2)
 
 
 p1 <- recordPlot()
@@ -50,17 +52,14 @@ invisible(dev.off())
 
 pdf("forest_plot_ic.pdf",
     width = 8, 
-    height = 8)
+    height = 10)
 p1
 dev.off()
 
 
-## IC sensitivity + TRT
+## IC sensitivity
 pdf(NULL)
 dev.control(displaylist = "enable")
-par(mfrow=c(2,1))
-layout(matrix(c(1,2), 2, 1, byrow = TRUE),
-       widths=c(1), heights = c(1, 0.57))  #c(1.775,1)
 
 metafor::forest(fit_internal_consistency_permuted_estimates_sensitivity,
                 transf = transf.iabt,
@@ -70,8 +69,23 @@ metafor::forest(fit_internal_consistency_permuted_estimates_sensitivity,
                 xlim = c(-1, 1.6),
                 at = c(0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
                 mlab = add_heterogeneity_metrics_to_forest(fit_internal_consistency_permuted_estimates_sensitivity))
-text(-1, 27, "Internal consistency", pos = 4)
-text(1.6, 27, bquote(paste(alpha, " [95% CI]")), pos = 2)
+text(-1, 38, "Internal consistency", pos = 4)
+text(1.6, 38, bquote(paste(alpha, " [95% CI]")), pos = 2)
+
+p1 <- recordPlot()
+invisible(dev.off())
+
+pdf("forest_plot_ic_insensitivity.pdf",
+    width = 8, 
+    height = 10)
+p1
+dev.off()
+
+
+
+## TRT
+pdf(NULL)
+dev.control(displaylist = "enable")
 
 metafor::forest(fit_test_retest_icc,
                 transf = transf.ztor,
@@ -87,11 +101,50 @@ text(1.82, 10, "ICC [95% CI]", pos = 2)
 p1 <- recordPlot()
 invisible(dev.off())
 
-pdf("forest_plots_ic_sensitivity_and_trt.pdf",
+pdf("forest_plot_trt.pdf",
     width = 8, 
-    height = 12)
+    height = 4.22)
 p1
 dev.off()
+
+# 
+# ## IC sensitivity + TRT
+# pdf(NULL)
+# dev.control(displaylist = "enable")
+# par(mfrow=c(2,1))
+# layout(matrix(c(1,2), 2, 1, byrow = TRUE),
+#        widths=c(1), heights = c(1, 0.47))  #c(1.775,1)
+# 
+# metafor::forest(fit_internal_consistency_permuted_estimates_sensitivity,
+#                 transf = transf.iabt,
+#                 xlab = bquote(paste("Cronbach's ", alpha)),
+#                 addcred = TRUE,
+#                 refline = FALSE,
+#                 xlim = c(-1, 1.6),
+#                 at = c(0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
+#                 mlab = add_heterogeneity_metrics_to_forest(fit_internal_consistency_permuted_estimates_sensitivity))
+# text(-1, 38, "Internal consistency", pos = 4)
+# text(1.6, 38, bquote(paste(alpha, " [95% CI]")), pos = 2)
+# 
+# metafor::forest(fit_test_retest_icc,
+#                 transf = transf.ztor,
+#                 xlab = "Absolute Agreement Interclass Correlation",
+#                 addcred = TRUE,
+#                 refline = FALSE,
+#                 xlim = c(-1.78, 1.82),
+#                 at = c(-0.4, -0.2, 0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
+#                 mlab = add_heterogeneity_metrics_to_forest(fit_test_retest_icc))
+# text(-1.78, 10, "Test-retest reliability", pos = 4)
+# text(1.82, 10, "ICC [95% CI]", pos = 2)
+# 
+# p1 <- recordPlot()
+# invisible(dev.off())
+# 
+# pdf("forest_plots_ic_sensitivity_and_trt.pdf",
+#     width = 8, 
+#     height = 16)
+# p1
+# dev.off()
 
 
 # gosh plots
