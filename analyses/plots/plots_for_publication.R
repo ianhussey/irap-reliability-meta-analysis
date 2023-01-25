4dc9fbaef0ae55de5f00af0d22b93a25abf006f4
ianhussey <ianhusseysipod@gmail.com>
2023-01-25 16:39:58 +0100

    updated analyses overall and trial type
    - replaced gosh plots with iterations of undue influence detection
    - reran analyses

--- analyses/plots/plots_for_publication.R
+++ analyses/plots/plots_for_publication.R
@@ -22,10 +22,6 @@ fit_internal_consistency_permuted_estimates <- read_rds("../models/fit_internal_
 fit_internal_consistency_permuted_estimates_sensitivity <- read_rds("../models/fit_internal_consistency_permuted_estimates_sensitivity.rds")
 fit_test_retest_icc <- read_rds("../models/fit_test_retest_icc.rds")
 
-plot_gosh_ic <- read_rds("../models/plot_gosh_ic.rds")
-plot_gosh_trt <- read_rds("../models/plot_gosh_trt.rds")
-# plot_gosh <- plot_gosh_ic + plot_gosh_trt + plot_layout(ncol = 2)  # combing them throws an error for some reason
-
 
 # forest plots
 
@@ -44,8 +40,8 @@ metafor::forest(fit_internal_consistency_permuted_estimates,
                 #ilab = data_internal_consistency_permuted_estimates$n,
                 #ilab.xpos = 1.1,
                 #ilab.pos = 2)
-text(-1, 41, "Internal consistency", pos = 4)
-text(1.6, 41, bquote(paste(alpha, " [95% CI]")), pos = 2)
+text(-1, 43, "Internal consistency", pos = 4)
+text(1.6, 43, bquote(paste(alpha, " [95% CI]")), pos = 2)
 #text(1.1, 37, expression(italic("N")), pos = 2)
 
 
@@ -71,8 +67,8 @@ metafor::forest(fit_internal_consistency_permuted_estimates_sensitivity,
                 xlim = c(-1, 1.6),
                 at = c(0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
                 mlab = add_heterogeneity_metrics_to_forest(fit_internal_consistency_permuted_estimates_sensitivity))
-text(-1, 39, "Internal consistency", pos = 4)
-text(1.6, 39, bquote(paste(alpha, " [95% CI]")), pos = 2)
+text(-1, 40, "Internal consistency", pos = 4)
+text(1.6, 40, bquote(paste(alpha, " [95% CI]")), pos = 2)
 
 p1 <- recordPlot()
 invisible(dev.off())
@@ -146,30 +142,3 @@ dev.off()
 #     height = 16)
 # p1
 # dev.off()
-
-
-# gosh plots
-pdf(NULL)
-dev.control(displaylist = "enable")
-plot_gosh_ic
-p1 <- recordPlot()
-invisible(dev.off())
-pdf("gosh_plot_ic.pdf",
-    width = 6, 
-    height = 6)
-p1
-dev.off()
-
-pdf(NULL)
-dev.control(displaylist = "enable")
-plot_gosh_trt
-p1 <- recordPlot()
-invisible(dev.off())
-pdf("gosh_plot_trt.pdf",
-    width = 6, 
-    height = 6)
-p1
-dev.off()
-
-
-
